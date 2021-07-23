41bf20e930fda734fc30283d8532ed5adf655d1b
BKavyasri <81008895+BKavyasri@users.noreply.github.com>
2021-07-23 22:00:21 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+$server="localhost";
+$user="root";
+$password="";
+$database="login";
+
+$conn=mysqli_connect($server,$user,$password,$database);
+
+if(!$conn){
+    echo " <script> alert('connection failed...') </script>";
+}
+
+?>
